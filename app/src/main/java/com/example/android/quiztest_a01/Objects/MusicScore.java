9e7255c87edc96361b5d5ae3a9a9902b2471514a
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-09-10 11:19:39 +0300

    Create MusicScore.java

---
+++ app/src/main/java/com/example/android/quiztest_a01/Objects/MusicScore.java
@@ -0,0 +1,49 @@
+package com.example.android.quiztest_a01.Objects;
+
+public class MusicScore {
+    private String userName;
+    private String score;
+    private String playlistId;
+    private String playlistName;
+    private String playListIcon;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(String playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public String getPlayListIcon() {
+        return playListIcon;
+    }
+
+    public void setPlayListIcon(String playListIcon) {
+        this.playListIcon = playListIcon;
+    }
+}
