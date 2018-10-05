2fca41b3a526becbb4d2d09b3256b288ad5eb7ad
Gian <gianlupds@gmail.com>
2018-10-05 10:42:09 +0200

    Updated firebase login -- needs a check to see if it's working - strange behavious with sign in in MainActivity line 126, seems like to start a second MainActivity instead of returning to the original one
    Added HisghscoreAdapter and HighscoreMessage
    - clicking the Button will send a highscoreMessage containing user's name
    and a Math.random() int value, just for test purpose. It doesn't seems to work
    because of permissions issues. I re downloaded google play services json, but nothing
    changed -- if you have time please verify this :)
    
    thank you

--- app/src/main/java/com/example/android/quiztest_a01/HighscoreActivity.java
+++ app/src/main/java/com/example/android/quiztest_a01/HighscoreActivity.java
@@ -6,9 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -22,11 +20,7 @@ import java.util.List;
 public class HighscoreActivity extends AppCompatActivity {
 
     // Views
-    Button highScoreButton;
-    EditText userNameEditText;
-    EditText highscoreValueEditText;
-    TextView highscoreTextView;
-    TextView nameTextView;
+    Button fakeHighscoreButton;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mHighscoreDatabaseReference;
     private ChildEventListener mChildEventListener;
@@ -46,15 +40,23 @@ public class HighscoreActivity extends AppCompatActivity {
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mHighscoreDatabaseReference = mFirebaseDatabase.getReference().child("highscore");
 
+        // this Button will retrieve the user's name and generate a random highscore
+        fakeHighscoreButton = findViewById(R.id.fake_highscore_button);
 
-        // Button to send userNameEditText & highscoreValueEditText values to the database
-        highScoreButton = findViewById(R.id.send_highscore_button);
-        userNameEditText = findViewById(R.id.user_name_edit_text);
-        highscoreValueEditText = findViewById(R.id.highscore_edit_text);
+        // when clicked it generates a fake highscore
+        fakeHighscoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // generating a fake highscore points
+                String newHighscore = Integer.toString((int) Math.random());
 
-        // Views for display back data from the FireBase (TEST)
-        highscoreTextView = findViewById(R.id.highscore_text_view);
-        nameTextView = findViewById(R.id.name_text_view);
+                // create a new highscore messagge with fake points and user's name
+                HighscoreMessage highscoreMessage = new HighscoreMessage(newHighscore, MainActivity.mUsername, null);
+
+                // push it to the database
+                mHighscoreDatabaseReference.push().setValue(highscoreMessage);
+            }
+        });
 
         // display highscore views
         mHighscoreListView = findViewById(R.id.highscore_listview);
@@ -63,29 +65,6 @@ public class HighscoreActivity extends AppCompatActivity {
         mHighscoreAdapter = new HighscoreAdapter(this, R.layout.highscore_message_layout, highscoreMessages);
         mHighscoreListView.setAdapter(mHighscoreAdapter);
 
-        highScoreButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // getting userNameEditText & highscoreValueEditText from views
-                String nameString = MainActivity.mUsername;
-                String highscoreString = highscoreValueEditText.getText().toString();
-
-                // create a new HighscoreMessage with those String values
-                HighscoreMessage highscoreMessage = new HighscoreMessage(nameString, highscoreString);
-                // push it to the database
-                mHighscoreDatabaseReference.push().setValue(highscoreMessage);
-
-                /**
-                 * IMPORTANT NOTE:
-                 * right now I changed the database .read and .write rules and you need to be athenticated
-                 * to .read and .write
-                 */
-
-                // then, clear all the EditText views
-                userNameEditText.clearComposingText();
-                highscoreValueEditText.clearComposingText();
-            }
-        });
 
         /** This is a listener to retrieve data from FireBase
          *  every time a new child is added (a new highscore is sended) it update the
@@ -96,9 +75,8 @@ public class HighscoreActivity extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
                 HighscoreMessage highScoreMessage = dataSnapshot.getValue(HighscoreMessage.class);
+                mHighscoreAdapter.add(highScoreMessage);
 
-                nameTextView.setText(highScoreMessage.getUsername());
-                highscoreTextView.setText(highScoreMessage.getHighscore());
             }
 
             @Override
